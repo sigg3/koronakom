0f3f721c906181b0533650bf3e31e4c833c64d66
Sigge Smelror <git@sigg3.net>
2020-11-24 12:32:42 +0100

    bugfix in routes.py wrt search engine params

--- routes.py
+++ routes.py
@@ -302,7 +302,7 @@ async def fritekst(request):
     await request.form()
     try:
         # print(request.query_params.keys())
-        ui = html.escape(request.query_params['sok'])
+        ui = html.escape(request.query_params['s'])
     except Exception as e:
         # could not get input ..? => bail!
         print(f"got exception: {e}")
