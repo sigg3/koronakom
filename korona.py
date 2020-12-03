4d2e2a4df2987428a77d86eccb9262336fa47f06
sigg3 <biamin@sigg3.net>
2020-12-03 19:28:35 +0100

    Bugfix in hinting for dl_csv_data()

--- korona.py
+++ korona.py
@@ -179,7 +179,7 @@ class Folkehelseinstituttet():
         return f"{self.git}/{self.fhi}/{self.surveillance_data}/{self.msis}"
 
 
-async def dl_csv_data(msis_url: str) -> Type(httpx.response:)
+async def dl_csv_data(msis_url: str) -> Type(httpx.response):
     """ Downloads msis data as csv """
     async with httpx.AsyncClient() as client:
         data_file = await client.get(msis_url)
