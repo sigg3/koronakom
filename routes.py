ec476b149c46dfe4005ae0a05d8a95c228dd6c92
Sigge Smelror <git@sigg3.net>
2020-12-04 18:59:24 +0100

    Sorting out norwegian xticks for mini_plot_trend

--- routes.py
+++ routes.py
@@ -171,8 +171,8 @@ def mini_plot_trend(kid:str) -> Type[bytes]:
     xtic_lab.reverse()
     xtics_nor = [ korona.norwegian_date(x, True) for x in xtics_nor ]
     xtics_nor = [ x.replace(" 2020","").replace(" 2021","") for x in xtics_nor ]
-    ax.set_xticks([xtic_lab[1], xtic_lab[-1]])
-    ax.set_xticklabels(xtics_nor)
+    ax.set_xticks(["", xtic_lab[1], xtic_lab[-1]])
+    ax.set_xticklabels(["", xtics_nor[0], xtics_nor[1])
     plt.yticks()
     plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
     plt.ylabel('')
