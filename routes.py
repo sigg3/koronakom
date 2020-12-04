7dc48356e744c243bc5b6179421cc3b288e83da3
Sigge Smelror <git@sigg3.net>
2020-12-04 19:39:08 +0100

    debug to check data against plot (debugging)

--- routes.py
+++ routes.py
@@ -178,12 +178,14 @@ def mini_plot_trend(kid:str) -> Type[bytes]:
     xtics_nor = [ korona.norwegian_date(x, True) for x in xtics_nor ]
     xtics_nor = [ x.replace(" 2020","").replace(" 2021","") for x in xtics_nor ]
     ax.set_xticks([xtic_lab[0], xtic_lab[-2]])
-    ax.set_xticklabels([xtics_nor])
+
 
     print(f"xtics_nor: {xtics_nor}")
     print(f"xtics_lab: {xtic_lab}")
     print(f"lab slice: {xtic_lab[1]},{xtic_lab[-1]}")
     print(f"use_tic: {use_tic}")
+
+    ax.set_xticklabels([xtics_nor[0], xtics_nor[1]])
 
     plt.yticks()
     plt.tick_params(axis='x', which='both', bottom=True, top=False, labelbottom=True)
