6d21494f567cc35cf41c232944c108d27535e810
Sigge Smelror <git@sigg3.net>
2020-12-11 13:43:59 +0100

    Adding list of datapoints to debug output

--- routes.py
+++ routes.py
@@ -237,6 +237,11 @@ async def debug_output_state(request):
     print("Debug: Outputting data dict to web viewer")
     dbgstr = ""
     for k,v in data.items(): dbgstr += f"{k} = {v} \n"
+    dbgstr += "datapoints:\n"
+    k = 0
+    for v in s.datapoints:
+        dbgstr += f"{k} = {v} \n"
+        k + =1
     return PlainTextResponse(f"{dbgstr}")
 
 
